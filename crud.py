d46d9c25ea034006b84eba816a85895a55d9a325
Tiela Black-Law <tiela.rose@gmail.com>
2023-05-09 20:56:03 -0700

    Create create_user function

---
+++ crud.py
@@ -0,0 +1,15 @@
+"""CRUD operations"""
+
+from model import db, User, Movie, Rating, connect_to_db
+
+def create_user(email, password):
+    """Create and return a new user."""
+
+    user = User(email=email, password=password)
+
+    return user
+    
+
+if __name__ == "__main__":
+    from server import app
+    connect_to_db(app)
