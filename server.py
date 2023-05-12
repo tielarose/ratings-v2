f78cfd54c88ab0433bde7d35e3bb5598872f3ae6
Tiela Black-Law <tiela.rose@gmail.com>
2023-05-11 21:00:37 -0700

    Create root route, homepage.html

--- server.py
+++ server.py
@@ -1,12 +1,21 @@
 """Server for movie ratings app."""
 
-from flask import Flask
+from flask import (Flask, render_template, request, flash, session,
+                   redirect)
+from model import connect_to_db, db
+import crud
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
 
+@app.route("/")
+def homepage():
 
-# Replace this with routes and view functions
+    return render_template("homepage.html")
+
 
 if __name__ == "__main__":
-
-    app.run(host="0.0.0.0", debug=True)
+    connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True, port=5002)
