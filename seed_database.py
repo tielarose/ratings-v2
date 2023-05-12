f2bebd74640beb952f99278bd89304c7cd44cc1e
Tiela Black-Law <tiela.rose@gmail.com>
2023-05-11 20:17:43 -0700

    Create seed_database.py, seed movies table with data from json file

---
+++ seed_database.py
@@ -0,0 +1,31 @@
+"""Seed database"""
+
+import os
+import json
+from random import choice, randint
+from datetime import datetime
+
+import crud
+import model
+import server
+
+os.system("dropdb ratings")
+os.system("createdb ratings")
+model.connect_to_db(server.app)
+model.db.create_all()
+
+with open('data/movies.json') as file:
+    movie_data = json.loads(file.read())
+
+movies_in_db = []
+for movie in movie_data:
+    overview, poster_path, title = (movie["overview"], movie["poster_path"], movie["title"])
+    release_date = datetime.strptime(movie["release_date"], "%Y-%m-%d")
+
+    current_movie = crud.create_movie(overview=overview,poster_path=poster_path,release_date=release_date,title=title)
+    movies_in_db.append(current_movie)
+
+
+model.db.session.add_all(movies_in_db)
+model.db.session.commit()
+
